3c120967b11f65f14c75e01434a07ff803bf3814
asyakasimova <kilyanet@gmail.com>
2016-02-24 11:27:00 +0300

    Домашнее задание 2. Расчет растояния через метод объекта p1

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -13,9 +13,17 @@ public class MyFirstProgram{
 
         Rectangle r = new Rectangle(4, 6);
         System.out.println("Площадь прямоуголоника со сторонами " + r.a + " и " + r.b + " равна " + r.area());
+
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(3, 4);
+
+        System.out.println("Расстояние от точки с координатами " + p1.x + "," + p1.y + " до точки с координатами " + p2.x + "," + p2.y + " = " + p1.distance (p2));
+
     }
 
     public static void hello(String somebody) {
        System.out.println("Hello, " + somebody + "!");
     }
+
+
 }
