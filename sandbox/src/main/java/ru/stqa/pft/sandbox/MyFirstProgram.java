6a6d30cf753007c2fbb9c26bbd2eb08eaa92a0bd
asyakasimova <kilyanet@gmail.com>
2016-02-24 11:29:04 +0300

    ДЗ 2. Убран лишний пробел

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -17,7 +17,7 @@ public class MyFirstProgram{
         Point p1 = new Point(0, 0);
         Point p2 = new Point(3, 4);
 
-        System.out.println("Расстояние от точки с координатами " + p1.x + "," + p1.y + " до точки с координатами " + p2.x + "," + p2.y + " = " + p1.distance (p2));
+        System.out.println("Расстояние от точки с координатами " + p1.x + "," + p1.y + " до точки с координатами " + p2.x + "," + p2.y + " = " + p1.distance(p2));
 
     }
 
