8ebd433cf83284127158f4fdf28dca4beea8c46e
asyakasimova <kilyanet@gmail.com>
2016-03-01 09:35:54 +0300

    Не рабочая версия - проблема с отсутствием static в login (SessionHelper)

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -0,0 +1,45 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import ru.stqa.pft.addressbook.GroupData11;
+
+/**
+ * Created by Valkyrja on 01.03.2016.
+ */
+public class GroupHelper {
+    private FirefoxDriver wd;
+
+
+    public GroupHelper(FirefoxDriver wd) {
+        this.wd = wd;
+    }
+
+    public void submitGroupCreation() {
+        wd.findElement(By.name("submit")).click();
+    }
+
+    public void fillGroupForm(GroupData11 groupData11) {
+        wd.findElement(By.name("group_name")).click();
+        wd.findElement(By.name("group_name")).clear();
+        wd.findElement(By.name("group_name")).sendKeys(groupData11.getName1());
+        wd.findElement(By.name("group_header")).click();
+        wd.findElement(By.name("group_header")).clear();
+        wd.findElement(By.name("group_header")).sendKeys(groupData11.getHeader1());
+        wd.findElement(By.name("group_footer")).click();
+        wd.findElement(By.name("group_footer")).clear();
+        wd.findElement(By.name("group_footer")).sendKeys(groupData11.getFooter1());
+    }
+
+    public void initGroupCreation() {
+        wd.findElement(By.name("new")).click();
+    }
+
+    public void deleteSelectedGroups() {
+        wd.findElement(By.xpath("//div[@id='content']/form/input[5]")).click();
+    }
+
+    public void selectGroup() {
+        wd.findElement(By.name("selected[]")).click();
+    }
+}
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/groupCreationTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.GroupData11;
+
+public class groupCreationTests extends  TestBase {
+
+    @Test
+    public void testGroupCreation() {
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().initGroupCreation();
+        app.getGroupHelper().fillGroupForm(new GroupData11("test1", "test2", "test3"));
+        app.getGroupHelper().submitGroupCreation();
+        app.getNavigationHelper().gotoGroupPage();
+    }
+
+}
