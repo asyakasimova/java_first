3b7f9ff63279aa59571aaec4059ca02055db5232
asyakasimova <kilyanet@gmail.com>
2016-03-03 08:48:52 +0300

    Revert "Наследование вспомогательных классов"
    This reverts commit 007503ff58c85bf9f86286ae1d94ee8003330dea.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/groupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/groupCreationTests.java
@@ -1,8 +1,34 @@
 package ru.stqa.pft.addressbook;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class groupCreationTests extends  TestBase {
+import java.util.concurrent.TimeUnit;
+
+public class groupCreationTests {
+    FirefoxDriver wd;
+    
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        wd.get("http://localhost/addressbook/group.php");
+        login("admin", "secret");
+    }
+
+    private void login(String username, String passwrd) {
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys(username);
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys(passwrd);
+        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
 
     @Test
     public void testGroupCreation() {
@@ -13,4 +39,41 @@ public class groupCreationTests extends  TestBase {
         gotoGroupPage();
     }
 
+    private void submitGroupCreation() {
+        wd.findElement(By.name("submit")).click();
+    }
+
+    private void fillGroupForm(GroupData11 groupData11) {
+        wd.findElement(By.name("group_name")).click();
+        wd.findElement(By.name("group_name")).clear();
+        wd.findElement(By.name("group_name")).sendKeys(groupData11.getName1());
+        wd.findElement(By.name("group_header")).click();
+        wd.findElement(By.name("group_header")).clear();
+        wd.findElement(By.name("group_header")).sendKeys(groupData11.getHeader1());
+        wd.findElement(By.name("group_footer")).click();
+        wd.findElement(By.name("group_footer")).clear();
+        wd.findElement(By.name("group_footer")).sendKeys(groupData11.getFooter1());
+    }
+
+    private void initGroupCreation() {
+        wd.findElement(By.name("new")).click();
+    }
+
+    private void gotoGroupPage() {
+        wd.findElement(By.linkText("groups")).click();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
+    
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
 }
