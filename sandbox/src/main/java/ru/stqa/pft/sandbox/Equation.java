6695716904c4f00c8b8c17a213ec3b367551e887
asyakasimova <kilyanet@gmail.com>
2016-03-07 12:40:10 +0300

    Тесты уравнений - не работают

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
@@ -0,0 +1,46 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Valkyrja on 07.03.2016.
+ */
+public class Equation {
+
+    private final double a;
+    private final double b;
+    private final double c;
+
+    private int n;
+
+    public Equation (double a, double b, double c){
+
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
+        double d = b*b - 4*a*c;
+
+        if (a == 0){
+            if (b == 0) {
+                if (c == 0) {
+                    n = -1;
+                }
+                n = 0;
+            }
+            n = 1;
+        } else {
+            if (d > 0) {
+                n = 2;
+            } else if (d == 0) {
+                n = 1;
+            } else {
+                n = 0;
+            }
+        }
+
+
+    }
+
+    public int rootNumber () {
+        return n;
+    }
+}
