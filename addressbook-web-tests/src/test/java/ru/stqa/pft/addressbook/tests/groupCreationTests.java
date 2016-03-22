d11e5d0ed347ed63c68c546091bbb18931ba5466
asyakasimova <kilyanet@gmail.com>
2016-03-22 22:43:47 +0300

    Тесты групп работают все

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/groupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/groupCreationTests.java
@@ -14,7 +14,7 @@ public class groupCreationTests extends TestBase {
 
         app.goTo().groupPage();
         List<GroupData> before = app.group().list();
-        GroupData group = new GroupData().withName("test2");
+        GroupData group = new GroupData().withName("test");
         app.group().create(group);
         List<GroupData> after = app.group().list();
         Assert.assertEquals(after.size(), before.size() + 1);
